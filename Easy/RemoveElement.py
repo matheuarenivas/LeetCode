cc972dd7a0506840f49b7280425cea05d21236f6
Matthew Arenivas <mrareniv@asu.edu>
2025-04-02 11:01:18 -0700

    Remove Element

---
+++ Easy/RemoveElement.py
@@ -0,0 +1,28 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        i = 0
+        j = len(nums)
+        k = 0
+
+        while i < j:
+            if nums[i] == val:
+                nums[i] = nums[j-1]
+                j-=1 
+            else:
+                i +=1
+        return j
+
+# Example usage:
+solution = Solution()
+nums = [3, 2, 2, 3]
+val = 3
+result = solution.removeElement(nums, val)
+print(result)  # Output: 2
+
+# Example usage:
+solution = Solution()
+nums = [0,1,2,2,3,0,4,2]
+val = 2
+result = solution.removeElement(nums, val)
+print(result)  # Output: 5
+
